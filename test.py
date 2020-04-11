594225ad19b94cd254b42acd9d19c391349e4e01
渡辺　昌 <maasa1221@gmail.com>
2020-04-12 00:43:38 +0900

    data add

--- test.py
+++ test.py
@@ -12,65 +12,96 @@ import pandas as pd
 import scipy.stats
 from sklearn.preprocessing import MinMaxScaler
 
-df0 = pd.read_csv("Log 2/new/fujitaRP1.csv", index_col=1)
-df2 = pd.read_csv("Log 2/new/koyamaRP1.csv", index_col=1)
-df3 = pd.read_csv("Log 2/new/koyanagiRP1.csv", index_col=1)
-df4 = pd.read_csv("Log 2/new/nimuraRP1.csv", index_col=1)
-df5 = pd.read_csv("Log 2/new/suzukiRP1.csv", index_col=1)
-df6_c = pd.read_csv("Log 2/new/64058436RP1.csv", index_col=1)
-df7_c = pd.read_csv("Log 2/new/79122114RP1.csv", index_col=1)
-df8_c = pd.read_csv("Log 2/new/52193348RP1.csv", index_col=1)
-df9_c = pd.read_csv("Log 2/new/19099510RP1.csv", index_col=1)
-df10_c = pd.read_csv("Log 2/new/19110622RP1.csv", index_col=1)
-df11_c = pd.read_csv("Log 2/new/19630516RP1.csv", index_col=1)
-df12_c = pd.read_csv("Log 2/new/82304270RP1.csv", index_col=1)
-df13_c = pd.read_csv("Log 2/new/15662472RP1.csv", index_col=1)
-df14_c = pd.read_csv("Log 2/new/19600594RP1.csv", index_col=1)
-df15_c = pd.read_csv("Log 2/new/73209319RP1.csv", index_col=1)
-df16_c = pd.read_csv("Log 2/new/18611860R_0.csv", index_col=1)
-df17_c = pd.read_csv("Log 2/new/19619780R_0.csv", index_col=1)
-df18_c = pd.read_csv("Log 2/new/19636280R0.csv", index_col=1)
-df19_c = pd.read_csv("Log 2/new/19639492R_0.csv", index_col=1)
-df20_c = pd.read_csv("Log 2/new/67090028R_0.csv", index_col=1)
-df21_c = pd.read_csv("Log 2/new/81160006R_0.csv", index_col=1)
-df22 = pd.read_csv("Log 2/masaru_0.csv", index_col=1)
-df23 = pd.read_csv("Log 2/etukoRP1.csv", index_col=1)
-df24 = pd.read_csv("Log 2/turu22.csv", index_col=1)
-# df25=pd.read_csv("Log 2/turu23.csv",index_col=1)
-df26 = pd.read_csv("Log 2/turu20.csv", index_col=1)
-df27 = pd.read_csv("Log 2/turu19.csv", index_col=1)
-df28 = pd.read_csv("Log 2/turu18.csv", index_col=1)
-#df29=pd.read_csv("Log 2/turu15.csv",index_col=1)
-#df30=pd.read_csv("Log 2/turu14_0.csv",index_col=1)
-df31 = pd.read_csv("Log 2/turu11.csv", index_col=1)
-df32 = pd.read_csv("Log 2/turu10.csv", index_col=1)
-df33 = pd.read_csv("Log 2/turu9_0.csv", index_col=1)
-#df34=pd.read_csv("Log 2/turu1.csv",index_col=1)
-df35_c = pd.read_csv("Log 2/18695075.csv", index_col=1)
-df36_c = pd.read_csv("Log 2/19025608.csv", index_col=1)
-df37_c = pd.read_csv("Log 2/19633531.csv", index_col=1)
-df38_c = pd.read_csv("Log 2/19638894.csv", index_col=1)
-df39_c = pd.read_csv("Log 2/88621754.csv", index_col=1)
+df0=pd.read_csv("data/new/fujitaRP1.csv",index_col=1)
+df2=pd.read_csv("data/new/koyamaRP1.csv",index_col=1)
+df3=pd.read_csv("data/new/koyanagiRP1.csv",index_col=1)
+df4=pd.read_csv("data/new/nimuraRP1.csv",index_col=1)
+df5=pd.read_csv("data/new/suzukiRP1.csv",index_col=1)
+df6_c=pd.read_csv("data/new/64058436RP1.csv",index_col=1)
+df7_c=pd.read_csv("data/new/79122114RP1.csv",index_col=1)
+df8_c=pd.read_csv("data/new/52193348RP1.csv",index_col=1)
+df9_c=pd.read_csv("data/new/19099510RP1.csv",index_col=1)
+df10_c=pd.read_csv("data/new/19110622RP1.csv",index_col=1)
+df11_c=pd.read_csv("data/new/19630516RP1.csv",index_col=1)
+df12_c=pd.read_csv("data/new/82304270RP1.csv",index_col=1)
+df13_c=pd.read_csv("data/new/15662472RP1.csv",index_col=1)
+df14_c=pd.read_csv("data/new/19600594RP1.csv",index_col=1)
+df15_c=pd.read_csv("data/new/73209319RP1.csv",index_col=1)
+df16_c=pd.read_csv("data/new/18611860R_0.csv",index_col=1) # ここ以降手首の角度あります。
+df17_c=pd.read_csv("data/new/19619780R_0.csv",index_col=1)
+df18_c=pd.read_csv("data/new/19636280R0.csv",index_col=1)
+df19_c=pd.read_csv("data/new/19639492R_0.csv",index_col=1)
+df20_c=pd.read_csv("data/new/67090028R_0.csv",index_col=1)
+df21_c=pd.read_csv("data/new/81160006R_0.csv",index_col=1)
+df22=pd.read_csv("data/masaru_0.csv",index_col=1)
+df23=pd.read_csv("data/etukoRP1.csv",index_col=1)
+df24=pd.read_csv("data/turu22.csv",index_col=1)
+#df25=pd.read_csv("data/turu23.csv",index_col=1)
+df26=pd.read_csv("data/turu20.csv",index_col=1)
+df27=pd.read_csv("data/turu19.csv",index_col=1)
+df28=pd.read_csv("data/turu18.csv",index_col=1)
+#df29=pd.read_csv("data/turu15.csv",index_col=1)
+#df30=pd.read_csv("data/turu14_0.csv",index_col=1)
+df31=pd.read_csv("data/turu11.csv",index_col=1)
+df32=pd.read_csv("data/turu10.csv",index_col=1)
+df33=pd.read_csv("data/turu9_0.csv",index_col=1)
+#df34=pd.read_csv("data/turu1.csv",index_col=1)
+df35_c=pd.read_csv("data/18695075.csv",index_col=1)
+df36_c=pd.read_csv("data/19025608.csv",index_col=1)
+df37_c=pd.read_csv("data/19633531.csv",index_col=1)
+df38_c=pd.read_csv("data/19638894.csv",index_col=1)
+df39_c=pd.read_csv("data/88621754.csv",index_col=1)
+df40_c=pd.read_csv("data/86140331_c.csv",index_col=1)
+df41_c=pd.read_csv("data/19639054_c.csv",index_col=1)
+df42_c=pd.read_csv("data/19629494_c.csv",index_col=1)
+df43_c=pd.read_csv("data/19169481_c.csv",index_col=1)
+df44_c=pd.read_csv("data/17167931_c.csv",index_col=1)
+df45_c=pd.read_csv("data/16651981_c.csv",index_col=1)
+df46_c=pd.read_csv("data/16639522_c.csv",index_col=1)
+df47_c=pd.read_csv("data/15620160_c.csv",index_col=1)
+df48=pd.read_csv("data/74123588.csv",index_col=1)
+df49=pd.read_csv("data/73043426.csv",index_col=1)
+df50=pd.read_csv("data/19653527.csv",index_col=1)
+df51=pd.read_csv("data/19652314.csv",index_col=1)
+df52=pd.read_csv("data/19651592.csv",index_col=1)
+df53=pd.read_csv("data/19648231.csv",index_col=1)
+df54=pd.read_csv("data/19630892.csv",index_col=1)
+df55=pd.read_csv("data/19140706.csv",index_col=1)
+df56=pd.read_csv("data/18693077.csv",index_col=1)
+df57=pd.read_csv("data/18642391.csv",index_col=1)
+df58_c=pd.read_csv("data/18684050_c.csv",index_col=1)
+df59_c=pd.read_csv("data/19627664_c.csv",index_col=1)
+df60_c=pd.read_csv("data/19639606_c.csv",index_col=1)
+df61_c=pd.read_csv("data/19641229_c.csv",index_col=1)
+df62_c=pd.read_csv("data/19648874_c.csv",index_col=1)
+df63_c=pd.read_csv("data/19650334_c.csv",index_col=1)
+df64_c=pd.read_csv("data/19662018_c.csv",index_col=1)
+df65_c=pd.read_csv("data/19680864_c.csv",index_col=1)
+df66_c=pd.read_csv("data/86141143_c.csv",index_col=1)
+df67_c=pd.read_csv("data/87175907_c.csv",index_col=1)
+df68=pd.read_csv("data/15012763.csv",index_col=1)
+df69=pd.read_csv("data/16605773.csv",index_col=1)
+df70=pd.read_csv("data/19632168.csv",index_col=1)
+df71=pd.read_csv("data/19674871.csv",index_col=1)
+df72=pd.read_csv("data/19684263.csv",index_col=1)
+df73=pd.read_csv("data/61176723.csv",index_col=1)
+df74=pd.read_csv("data/79303810.csv",index_col=1)
+df75=pd.read_csv("data/85226517.csv",index_col=1)
+df76_c=pd.read_csv("data/20204/5299463_c.csv",index_col=1)
+df77_c=pd.read_csv("data/20204/19642763_c.csv",index_col=1)
+df77_c=pd.read_csv("data/20204/19679037_c.csv",index_col=1)
+df79_c=pd.read_csv("data/20204/19684183_c.csv",index_col=1)
+df80_c=pd.read_csv("data/20204/20039931_c.csv",index_col=1)
+df81_c=pd.read_csv("data/20204/20050392_c.csv",index_col=1)
+df82=pd.read_csv("data/20204/19628315.csv",index_col=1)
+df83=pd.read_csv("data/20204/19652261.csv",index_col=1)
+df84=pd.read_csv("data/20204/19698855.csv",index_col=1)
+df85=pd.read_csv("data/20204/19702573.csv",index_col=1)
+df86=pd.read_csv("data/20204/20602448.csv",index_col=1)
+df87=pd.read_csv("data/20204/20606649.csv",index_col=1)
+df88=pd.read_csv("data/20204/76066920.csv",index_col=1)
+df89=pd.read_csv("data/20204/85293695.csv",index_col=1)
 
-df40_c = pd.read_csv("Log 2/86140331_c.csv", index_col=1)
-df41_c = pd.read_csv("Log 2/19639054_c.csv", index_col=1)
-df42_c = pd.read_csv("Log 2/19629494_c.csv", index_col=1)
-df43_c = pd.read_csv("Log 2/19169481_c.csv", index_col=1)
-df44_c = pd.read_csv("Log 2/17167931_c.csv", index_col=1)
-df45_c = pd.read_csv("Log 2/16651981_c.csv", index_col=1)
-df46_c = pd.read_csv("Log 2/16639522_c.csv", index_col=1)
-df47_c = pd.read_csv("Log 2/15620160_c.csv", index_col=1)
-
-df48 = pd.read_csv("Log 2/74123588.csv", index_col=1)
-df49 = pd.read_csv("Log 2/73043426.csv", index_col=1)
-df50 = pd.read_csv("Log 2/19653527.csv", index_col=1)
-df51 = pd.read_csv("Log 2/19652314.csv", index_col=1)
-df52 = pd.read_csv("Log 2/19651592.csv", index_col=1)
-df53 = pd.read_csv("Log 2/19648231.csv", index_col=1)
-df54 = pd.read_csv("Log 2/19630892.csv", index_col=1)
-df55 = pd.read_csv("Log 2/19140706.csv", index_col=1)
-df56 = pd.read_csv("Log 2/18693077.csv", index_col=1)
-df57 = pd.read_csv("Log 2/18642391.csv", index_col=1)
 
 
 def ffts(array, number_tree):
@@ -106,10 +137,10 @@ for z in range(1):
     dt = 1/60  # data step [s]
     train_data_contena = []
     train_target_contena = []
-    normal_counter = 25  # healthy people of number
+    normal_counter = 21  # healthy people of number
     counter = 0
-    human_list = [df0, df27, df2, df5, df3, df4, df22, df23, df24, df26, df27, df28, df31, df32, df33, df48, df49, df50, df51, df52, df53, df54, df55, df56, df57, df6_c, df7_c, df8_c, df9_c, df10_c,
-                  df11_c, df12_c, df13_c, df14_c, df15_c, df35_c, df36_c, df37_c, df38_c, df39_c, df16_c, df17_c, df19_c, df18_c, df20_c, df21_c, df40_c, df41_c, df42_c, df43_c, df44_c, df45_c, df46_c, df47_c]
+    human_list = [df0, df27, df2, df5, df3, df4, df22, df23, df24, df26, df27, df28, df31, df32, df33, df48, df49, df50, df51, df52, df53, df6_c, df7_c, df8_c, df9_c, df10_c,
+                  df11_c, df12_c, df13_c, df14_c, df15_c, df35_c, df36_c, df37_c, df38_c, df39_c, df16_c, df17_c, df19_c, df18_c, df20_c, df21_c, df40_c,df45_c, df46_c, df47_c]
     a = 64
     mm = MinMaxScaler()
 
@@ -134,11 +165,13 @@ for z in range(1):
             train_target_contena.append(np.array(target_box))
             counter += 1
 
+        counter = 0
+        normal_counter = 4
         test_target_contena = []
         test_data_contena = []
-        test_list = [df0]
+        test_list = [df54, df55, df56, df57,df41_c, df42_c, df43_c, df44_c, ]
         test_data_regista = np.zeros((nnn, a))
-        counter = 0
+        
 
         for i in test_list:
             train_box = []
@@ -152,25 +185,32 @@ for z in range(1):
 
         train_data_contena = np.array(np.abs(train_data_contena))
         test_data_contena = np.array(np.abs(test_data_contena))
-        train_data_contena = train_data_contena.reshape(54*reshape_base, 1*a)
+        train_data_contena = train_data_contena.reshape(46*reshape_base, 1*a)
+        test_data_contena = test_data_contena.reshape(8*reshape_base, 1*a)
         train_target_contena = np.array(
-            train_target_contena).reshape(54*reshape_base)
+            train_target_contena).reshape(46*reshape_base)
+        test_target_contena = np.array(
+            test_target_contena).reshape(8*reshape_base)
         # train_data_contena2 = mm.fit_transform(train_data_contena)
         train_data_contena2 = train_data_contena
-        clf = RandomForestClassifier(max_depth=5)
+        clf = RandomForestClassifier()
         pca = PCA(n_components=2)
         X = pca.fit_transform(train_data_contena2)
-        stratifiedkfold = StratifiedKFold(n_splits=6)
-        pred_score = cross_val_score(
-            clf, X, train_target_contena, cv=stratifiedkfold)
-        pred_score = pred_score.mean()
-        pred_vec = cross_val_predict(
-            clf, X, train_target_contena, cv=stratifiedkfold)
-
-        if first_score < pred_score:
-            first_score = pred_score
-            first_vec = pred_vec
-            first_tree_num = j
+        X_t = pca.fit_transform(test_data_contena)
+        
+        # stratifiedkfold = StratifiedKFold(n_splits=6)
+        # pred_score = cross_val_score(
+        #     clf, X, train_target_contena, cv=stratifiedkfold)
+        # pred_score = pred_score.mean()
+        # pred_vec = cross_val_predict(
+        #     clf, X, train_target_contena, cv=stratifiedkfold)
+        clf.fit(X,train_target_contena)
+        # clf.predict(X_t)
+        print("aaa")
+        print( clf.predict(X_t))
+        print( clf.score(X_t,test_target_contena))
+        print("bbb")
+       
 
     idx = func1(first_vec == train_target_contena, False)
     print(first_score, first_tree_num)
@@ -207,3 +247,8 @@ for z in range(1):
     score_list = [first_score, second_score]
 print(tree_list)
 print(score_list)
+
+
+
+
+
